924adc6cf9735e19e6c825ae1b13aa62fd156bb0
Gary Fagan <garyfagan1987@googlemail.com>
2019-07-14 09:11:12 +0100

    adding some logging

--- controllers/user.js
+++ controllers/user.js
@@ -35,22 +35,27 @@ function validate(user) {
 }
 
 const validateRequestBody = (req) => {
+  console.log('2');
   const { error } = validate(req.body);
   if (error) throw Error(error.details[0].message);
 };
 
 const hasExistingAccount = async (req) => {
+  console.log('3');
   const user = await User.findOne({ email: req.body.email });
   if (user) throw Error('User is already registered.');
 };
 
 const createAccount = async (req) => {
+  console.log('4');
   const user = new User(_.pick(req.body, ['name', 'email', 'password']));
   try {
+    console.log('5');
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
   } catch (error) {
+    console.log('6');
     if (error) throw Error('User could not be created');
   }
 };
@@ -62,10 +67,12 @@ exports.get = async (req, res) => {
 
 exports.create = async (req, res) => {
   try {
+    console.log('1');
     validateRequestBody(req, res);
     await hasExistingAccount(req);
     await createAccount(req, res);
   } catch (error) {
+    console.log('7');
     if (error && dev) debug(`ERROR | ${req.method} | ${req.url}`);
     return res.status(400).send(error.message);
   }
