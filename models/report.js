07e9a0d6bd5779a5b40cd08d8868fbe130f8bd33
Gary Fagan <garyfagan1987@googlemail.com>
2019-08-06 08:34:56 +0100

    adding net item total

--- models/report.js
+++ models/report.js
@@ -7,6 +7,7 @@ const sheetSchema = new mongoose.Schema({
   items: { required: true, type: Array },
   title: { required: true, type: String },
   totalGross: { required: true, type: String },
+  totalNet: { required: true, type: String },
   totalVat: { required: true, type: String },
 });
 
